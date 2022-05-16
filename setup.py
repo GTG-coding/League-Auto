459aa316a799e70c2c9bc7ad69659066a2261b39
GTG <jwideman15@gmail.com>
2022-05-16 00:18:30 -0400

    changed pywin version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", package])
+    subprocess.check_call([sys.executable,"-m", "pip", "install", package])
 
 
 contents = open("requirements.txt","r+")
